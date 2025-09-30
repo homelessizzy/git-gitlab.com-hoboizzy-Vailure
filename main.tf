1f633cd5f4f2dd67e6daea7d2df3b7924df79559
homelessizzy <homelessizzy@gmail.com>
2025-09-30 01:38:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 
+terraform {
+  backend "remote" {
+    organization = "your-terraform-org"
+    workspaces {
+      name = "your-workspace"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
